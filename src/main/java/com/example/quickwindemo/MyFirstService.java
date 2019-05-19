620ae9fbcba2fe9403b328db9bb59eee352d807f
chris <christian.oette@web.de>
2019-05-19 14:40:18 +0200

    Add first quick-win service

---
+++ src/main/java/com/example/quickwindemo/MyFirstService.java
@@ -0,0 +1,18 @@
+package com.example.quickwindemo;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyFirstService {
+
+    public MyFirstService() {
+        System.out.println("MyFirstService constructor called");
+    }
+
+    @EventListener(classes = ApplicationReadyEvent.class)
+    public void init() {
+        System.out.println("MyFirstService is ready. Application has been started.");
+    }
+}
---
+++ src/main/java/com/example/quickwindemo/QuickWinDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.quickwindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickWinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickWinDemoApplication.class, args);
+	}
+
+}
