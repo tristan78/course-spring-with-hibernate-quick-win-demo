1cfed4469eaba09f2d365fbe8b64085761da1e05
Tristan78 <tristan78@gmx.de>
2020-10-10 23:24:28 +0200

    After unit 13. Controller und Service kombinieren

---
+++ src/main/java/com/example/quickwindemo/HelloDto.java
@@ -0,0 +1,13 @@
+package com.example.quickwindemo;
+
+public class HelloDto {
+    private String text;
+
+    public HelloDto(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
--- src/main/java/com/example/quickwindemo/MyFirstService.java
+++ src/main/java/com/example/quickwindemo/MyFirstService.java
@@ -3,6 +3,7 @@ package com.example.quickwindemo;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class MyFirstService {
@@ -14,5 +15,12 @@ public class MyFirstService {
     @EventListener(classes = ApplicationReadyEvent.class)
     public void init(){
         System.out.println("MyFirstService is ready. Application has been started.");
+    }
+
+    static int counter = 0;
+
+    public HelloDto hello(String user) {
+        counter++;
+        return new HelloDto(String.format("Hello %s, this application has been called %s times", user, counter));
     }
 }
